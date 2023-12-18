625df0f378b186b5658cd86d82691e6c6ff5e7cf
Abhishek_ <vsukumar0991@gmail.com>
2023-12-18 19:28:27 +0530

    Code changes has been added

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,19 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+    private int price;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        super.addExtraCheese();
+        super.addExtraToppings();
+        super.addTakeaway();
+        this.price += super.getPrice();
     }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,17 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean cheese = false;
+    private boolean toppings  = false;
+    private boolean takeaway = false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price  = 300;
+        }
+        else
+            this.price = 400;
     }
 
     public int getPrice(){
@@ -17,18 +25,49 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(cheese==false) {
+            this.price += 80;
+            cheese = true;
+        }
+        else
+            return;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg) {
+            if(toppings == false) {
+                this.price += 70;
+                toppings = true;
+            }
+            else
+                return;
+        }
+        else {
+            if(toppings == false) {
+                this.price += 120;
+                toppings = true;
+            }
+            else
+                return;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(takeaway == false) {
+            this.price += 20;
+            takeaway = true;
+            return;
+        }
+        else
+            return;
+
+
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return "Total Price: "+ this.price;
     }
 }
