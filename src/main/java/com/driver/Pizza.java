39fc90c6b0917fa17d06d98ff6b6b25b028ed3c8
Abhishek_ <vsukumar0991@gmail.com>
2023-12-20 19:08:12 +0530

    Code has been added

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,6 +2,7 @@ package com.driver;
 
 public class Pizza {
 
+    private int basePrice;
     private int price;
     private Boolean isVeg;
     private String bill;
@@ -15,11 +16,13 @@ public class Pizza {
         this.isVeg = isVeg;
         // your code goes here
         if(isVeg){
+            basePrice  = 300;
             this.price  = 300;
             cheesePrice  = 80;
             toppingPrice  = 70;
         }
         else {
+            basePrice  = 400;
             this.price = 400;
             cheesePrice  = 80;
             toppingPrice  = 120;
@@ -34,34 +37,40 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(cheese == false){
+            this.price += this.cheesePrice;
+        }
         this.cheese  = true;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(toppings==false){
+            this.price += this.toppingPrice;
+        }
         toppings = true;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(takeaway==false){
+            this.price += takeAwayPrice;
+        }
         takeaway = true;
 
     }
 
     public String getBill(){
         // your code goes here
-        bill  = "Base Price Of The Pizza: "+this.price+ "\n";
+        bill  = "Base Price Of The Pizza: "+basePrice+ "\n";
         if(cheese){
             bill += "Extra Cheese Added: "+this.cheesePrice+"\n";
-            this.price += cheesePrice;
         }
         if(toppings){
             bill += "Extra Toppings Added: " +this.toppingPrice+"\n";
-            this.price += toppingPrice;
         }
         if(takeaway){
             bill += "Paperbag Added: "+this.takeAwayPrice+"\n";
-            this.price += takeAwayPrice;
         }
         bill += "Total Price: "+this.price+"\n";
         return bill;
